0247fc900fb57d4f663007be33df661c938896a4
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2022-07-11 10:54:24 +0200

    Adding CountingNumbers project.

---
+++ CountingNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a integer number:");
+var input = Console.ReadLine()!;
+try
+{
+  var len = Math.Round(Math.Log10(int.Parse(input)))+1;
+  Console.ForegroundColor = ConsoleColor.Green;
+  Console.WriteLine($"{input} contains {len} numbers.");
+}
+catch
+{
+  Console.ForegroundColor = ConsoleColor.Red;
+  Console.WriteLine($"{input} is not integer number.");
+}
