457a14bef03699cd413b9a4e8bec12dbfec89783
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2022-07-13 23:14:58 +0200

    Simplified message.

--- CountingNumbers/Program.cs
+++ CountingNumbers/Program.cs
@@ -1,16 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Enter a number:");
 var input = Console.ReadLine()!;
 var len = input
   .Where(char.IsDigit)
   .Count();
-if (len > 0)
-{
-  Console.ForegroundColor = ConsoleColor.Green;
-  Console.WriteLine($"{input} contains {len} numbers.");
-}
-else
-{
-  Console.ForegroundColor = ConsoleColor.Red;
-  Console.WriteLine($"{input} don't contains an numbers.");
-}
+
+Console.ForegroundColor = len != 0 ? ConsoleColor.Green : ConsoleColor.Red;
+Console.WriteLine($"{input} contains {len} numbers.");
